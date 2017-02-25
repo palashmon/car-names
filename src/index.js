d57bb8cc0ea482982ecc2918cbb8e79302c8fe17
Palash Mondal <palashm@live.in>
2017-02-25 16:18:36 +0530

    Travis build fail test, using undefined function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const randomArray = require('unique-random-array');
 // Load the car names array from json file
 const carNames = require('./car-names.json');
 
+// Travis build fail test. Use undefined function here
+carwWithRandomName(); 
+
 // Object exported when car-names module is loaded
 module.exports = {
 	all: carNames,
