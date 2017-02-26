69e4c38fb0f91a53ab3edf6f31dd4c98c24349fc
Palash Mondal <palashm@live.in>
2017-02-26 23:07:45 +0530

    Add ES6 support and ghooks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-// Load the module unique-random-array module
-const randomArray = require('unique-random-array');
+import uniqueRandomArray from 'unique-random-array';
+import carNames from './car-names.json';
 
-// Load the car names array from json file
-const carNames = require('./car-names.json');
-
-// Object exported when car-names module is loaded
 module.exports = {
 	all: carNames,
-	random: randomArray(carNames)
+	random: uniqueRandomArray(carNames)
 }
