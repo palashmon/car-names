31a5a29bf3d531c026e39fa3d6191f89153966c3
Palash Mondal <14850464+palashmon@users.noreply.github.com>
2026-02-09 11:26:27 +0530

    fix: build issue with identifier assert

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import uniqueRandomArray from "unique-random-array";
-import carNames from "./car-names.json" assert { type: "json" };
+import fs from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const configPath = join(dirname(fileURLToPath(import.meta.url)), "./car-names.json");
+const carNames = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
 export default {
   all: carNames,
